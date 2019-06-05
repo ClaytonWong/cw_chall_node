eb3c1500c9bfe18ae18557b0779170c5869ff8da
Clayton <c_w_serious-github@yahoo.com.au>
2019-06-05 21:28:03 +1000

    RENAME getRequest to getFromZendeskAPI

--- request.js
+++ request.js
@@ -1,14 +1,14 @@
 const zendesk = require('./api/zendesk')
 
-function getRequest(restOfURL) {
-  zendesk.getReq(restOfURL)
+function getFromZendeskAPI(restOfURL) {
+  zendesk.getFromAPI(restOfURL)
     .then(res => {
       var page = res.data
       console.log(page.count);
     })
     .catch((error) => {
-      console.log('error from .catch in getRequest definition: ', error)
+      console.log('error from .catch in getFromZendeskAPI definition: ', error)
     })
 }
 
-getRequest('tickets.json?per_page=25');
+getFromZendeskAPI('tickets.json?per_page=25');
