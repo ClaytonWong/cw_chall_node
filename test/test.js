500e8cde66db0bae1fdd74749c960f532cf0d0f0
Clayton <c_w_serious-github@yahoo.com.au>
2019-06-11 22:23:57 +1000

    ADD Chai

--- test/test.js
+++ test/test.js
@@ -1,12 +1,30 @@
-var supertest = require("supertest");
-var should = require("should");
-const getFromZendeskAPI = require("../api/zendesk");
+//var request = require("supertest");
+//var should = require("should");
+var expect = require('chai').expect;
+const zendesk = require("../api/zendesk");
+const app = zendesk.getFromAPI;
 
-describe('Authentication', function() {
+/*
+describe('GET', function() {
+  it('responds', function() {
+    return request(app)
+      .get('tickets.json?page=1&per_page=25')
+      .set('Accept', 'application/json')
+      //.expect('Content-Type', /json/)
+      //.expect(200)
+      .then(response => {
+          assert(response.data.count, 101)
+      })
+  });
+});
+*/
 
-  it('errors if wrong basic auth', function(done) {
-    getFromZendeskAPI.get()
-    .auth('incorrect', 'credentials')
-    .expect(401, done)
+describe('GET tickets.json', function () {
+  it('respond with json ', function (done) {
+      request(app)
+          .get('tickets/1.json')
+          //.set('Accept', 'application/json')
+          //.expect('Content-Type', /json/)
+          .expect(404, done);
   });
 });
