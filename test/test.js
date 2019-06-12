be8ee05222d7f7b074f45a4a2d66dac0b90e5412
Clayton <c_w_serious-github@yahoo.com.au>
2019-06-12 12:50:13 +1000

    can test getNumOfTickets function from request.js

--- test/test.js
+++ test/test.js
@@ -4,17 +4,34 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 const zendesk = require("../api/zendesk");
+const req = require("../request");
 const app = zendesk.getFromAPI;
 
 // Check if there are 101 tickets in account as of Tue 2019 Jun 11
 it('should do something with promises', function() {
-  var value = app('tickets.json')
+  var value = app('tickets.json');
 
   return expect(value.then(o => o.data.count)).to.eventually.equal(101);
 });
 
 it('should do something with promises test 2', function() {
-  var value = app('tickets.json')
+  var value = app('tickets.json');
 
   return expect(value.then(o => o.data.count)).to.eventually.not.equal(102);
+});
+
+
+// Test getNumOfTickets function from request.js
+const getTicketCount = req.getNumOfTickets;
+
+it('Number of tickets should equal 101', function() {
+  var value = getTicketCount();
+
+  return expect(value.then()).to.eventually.equal(101);
+});
+
+it('Number of tickets should not equal 102', function() {
+  var value = getTicketCount();
+
+  return expect(value.then()).to.eventually.not.equal(102);
 });
