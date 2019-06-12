cb7735214d725fbee01fd34a78f190f93d08bb60
Clayton <c_w_serious-github@yahoo.com.au>
2019-06-12 14:28:24 +1000

    can pass 8 tests

--- test/test.js
+++ test/test.js
@@ -8,33 +8,33 @@ const req = require("../request");
 const app = zendesk.getFromAPI;
 
 // Check if there are 101 tickets in account as of Tue 2019 Jun 11
-// it('should do something with promises', function() {
-//   var value = app('tickets.json');
+it('should do something with promises', function() {
+  var value = app('tickets.json');
 
-//   return expect(value.then(o => o.data.count)).to.eventually.equal(101);
-// });
+  return expect(value.then(o => o.data.count)).to.eventually.equal(101);
+});
 
-// it('should do something with promises test 2', function() {
-//   var value = app('tickets.json');
+it('should do something with promises test 2', function() {
+  var value = app('tickets.json');
 
-//   return expect(value.then(o => o.data.count)).to.eventually.not.equal(102);
-// });
+  return expect(value.then(o => o.data.count)).to.eventually.not.equal(102);
+});
 
 
 // Test getNumOfTickets function from request.js
-// const getTicketCount = req.getNumOfTickets;
+const getTicketCount = req.getNumOfTickets;
 
-// it('Number of tickets should equal 101', function() {
-//   var value = getTicketCount();
+it('Number of tickets should equal 101', function() {
+  var value = getTicketCount();
 
-//   return expect(value.then()).to.eventually.equal(101);
-// });
+  return expect(value.then()).to.eventually.equal(101);
+});
 
-// it('Number of tickets should not equal 102', function() {
-//   var value = getTicketCount();
+it('Number of tickets should not equal 102', function() {
+  var value = getTicketCount();
 
-//   return expect(value.then()).to.eventually.not.equal(102);
-// });
+  return expect(value.then()).to.eventually.not.equal(102);
+});
 
 
 // Test getFromZendeskAPI function from request.js
@@ -53,4 +53,19 @@ it('1 page should not have a maximum of 26 tickets', function() {
   var value = getFromZendeskAPI(`tickets.json?page=${pageNum}&per_page=${ticketsPerPage}`);
 
   return expect(value.then(o => o.data.tickets.length)).to.eventually.not.equal(26);
+});
+
+let id = 1;
+
+it('Should get ticket with id of 1 if you ask for it', function() {
+  // Test part of showDetailsForOneTicket function from request.js
+  var value = getFromZendeskAPI(`tickets/${id}.json`);
+
+  return expect(value.then(o => o.data.ticket.id)).to.eventually.equal(1);
+});
+
+it('Should not get ticket with id of 26 if you ask for ticket with id of 1', function() {
+  var value = getFromZendeskAPI(`tickets/${id}.json`);
+
+  return expect(value.then(o => o.data.ticket.id)).to.eventually.not.equal(26);
 });
